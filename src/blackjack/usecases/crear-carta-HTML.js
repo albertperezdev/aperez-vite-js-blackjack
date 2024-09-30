3f5f2be31f5425488e5cf923b96ba56d79489ee6
Alberto Perez <albertperez.dev@gmail.com>
2024-09-29 22:22:38 -0500

    derectory change- assets/cartas- ./public/assets/cartas

--- src/blackjack/usecases/crear-carta-HTML.js
+++ src/blackjack/usecases/crear-carta-HTML.js
@@ -4,7 +4,7 @@ export const crearCarta = (carta) => {
     if (!carta) throw new Error ('El argumento carta es obligatorio')
     
         const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+        imgCarta.src = `./public/assets/cartas/${ carta }.png`; //3H, JD
         imgCarta.classList.add('carta');
         
         return imgCarta
